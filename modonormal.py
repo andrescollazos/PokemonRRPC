b4abc8059076744a0ed4b943e37c98676a1e38da
andrescollazos <pipecollazos1@gmail.com>
2016-05-20 18:37:35 -0500

    Las imagenes de los mapas son cargadas y escaladas para adaptarse a la pantalla

--- modonormal.py
+++ modonormal.py
@@ -1,6 +1,11 @@
 import pygame
 import ConfigParser
 
+# Constantes
+BLANCO = (255, 255, 255)
+NEGRO = (0, 0, 0)
+tamPantalla = [527, 398]
+
 class Mapa(object):
 	def __init__(self, filename):#, jugador):
 		# Elementos propios del mapa:
@@ -23,9 +28,12 @@ class Mapa(object):
 		# Obtener dimensiones del mapa
 		imagen_ancho, imagen_alto = self.image.get_size()
 		self.matrizMap = [] # Matriz que relaciona el mapa con la imagen
-		scale = parser.get("level", "scale") # Saber el tamanyo de cada elemento
-		ancho = 32/int(scale)
-		alto = 32/int(scale)
+		self.scale = int(parser.get("level", "scale")) # Saber el tamanyo de cada elemento
+		# Escalar la imagen:
+		self.image = pygame.transform.scale(self.image, (imagen_ancho*self.scale, imagen_alto*self.scale))
+		imagen_ancho, imagen_alto = self.image.get_size()
+		ancho = 32/self.scale
+		alto = 32/self.scale
 		for fondo_x in range(0, imagen_ancho/ancho):
 			linea = []
 			self.matrizMap.append(linea)
@@ -43,9 +51,13 @@ class Mapa(object):
 		except KeyError:
 			return {}
 
-
+# MAIN
 if __name__=='__main__':
-	filename ="maps/interior.map"
+	pygame.init()
+	pantalla = pygame.display.set_mode(tamPantalla)
+	pantalla.fill(NEGRO)
+
+	filename ="maps/ruta1.map"
 	ciudadVerde = Mapa(filename)
 
 	print "--------------------------------------------------------------------"
@@ -55,3 +67,17 @@ if __name__=='__main__':
 		for y in x:
 			print y,
 		print ""
+
+	# Ciclo del juego
+	terminar = False
+	x, y = ciudadVerde.image.get_size()
+	x, y = x/32, y/32
+	while not terminar:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				terminar = True
+		for j in range(0, y):
+			for i in range(0, x):
+				pantalla.blit(ciudadVerde.matrizMap[i][j], (i*(32/ciudadVerde.scale), j*(32/ciudadVerde.scale)))
+
+		pygame.display.flip()
